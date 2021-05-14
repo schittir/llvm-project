684021338a4d6ed1cc1c311d1a8d30dc6b29dac3
Sindhu Chittireddy <sindhu.chittireddy@intel.com>
2021-05-14 12:08:53 -0700

    Edit parsing test per review comments

--- clang/test/ParserSYCL/unique_stable_name.cpp
+++ clang/test/ParserSYCL/unique_stable_name.cpp
@@ -1,33 +1,37 @@
 // RUN: %clang_cc1 -fsyntax-only -verify -Wno-unused %s
 
-namespace NS{};
+namespace NS {
+int good = 55;
+}
 
 void f(int var) {
   // expected-error@+1{{expected '(' after '__builtin_unique_stable_name'}}
-  __builtin_unique_stable_name int;
+  __builtin_unique_stable_name int; // Correct usage is __builtin_unique_stable_name(int);
+
   // expected-error@+1{{expected '(' after '__builtin_unique_stable_name'}}
-  __builtin_unique_stable_name {int};
+  __builtin_unique_stable_name {int}; // Correct usage is __builtin_unique_stable_name(int);
+
+  // expected-error@+2{{expected ')'}}
+  // expected-note@+1{{to match this '('}}
+  __builtin_unique_stable_name(int; //Missing paren before semicolon
+
+  // expected-error@+2{{expected ')'}}
+  // expected-note@+1{{to match this '('}}
+  __builtin_unique_stable_name(int, float); // Missing paren before comma
 
   __builtin_unique_stable_name(var);
-  // expected-error@+1{{use of undeclared identifier 'bad_var'}}
-  __builtin_unique_stable_name(bad_var);
-  // expected-error@+1{{use of undeclared identifier 'bad'}}
-  __builtin_unique_stable_name(bad::type);
-  // expected-error@+1{{no member named 'still_bad' in namespace 'NS'}}
-  __builtin_unique_stable_name(NS::still_bad);
+  __builtin_unique_stable_name(NS::good);
 }
 
 template <typename T>
 void f2() {
-  // expected-error@+1{{no member named 'bad_val' in 'S'}}
-  __builtin_unique_stable_name(T::bad_val);
-  // expected-error@+1{{no type named 'bad_type' in 'S'}}
-  __builtin_unique_stable_name(typename T::bad_type);
+  __builtin_unique_stable_name(typename T::good_type);
 }
 
-struct S{};
+struct S {
+  class good_type {};
+};
 
 void use() {
-  // expected-note@+1{{in instantiation of}}
   f2<S>();
 }
